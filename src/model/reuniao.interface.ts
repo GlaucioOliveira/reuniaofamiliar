e1b6f34ffbf6638109729e7ff20b47d27bb98ca9
Gláucio Oliveira <glaucio.dev@gmail.com>
2024-08-14 01:13:08 -0600

    refactor, angular upgrade and enhancements

---
+++ src/model/reuniao.interface.ts
@@ -0,0 +1,30 @@
+import { format, parseISO } from "date-fns";
+import { concat } from "rxjs";
+import { Hino } from "./hino.interface";
+
+export class ReuniaoFamiliar {
+    datareuniaoStr: string = '';
+    datareuniao?: Date = new Date();
+    naomembro?: boolean = false;
+    dirigidapor: number = 0;
+    primeira_oracao: number = 0;
+    primeiro_hino?: number;
+    primeiro_hino_nome: string = '';
+    mensagem: number = 0;
+    ultima_oracao: number = 0;
+    numero: number = 0;
+    anuncio: string = '';
+    observacao: string = '';
+    Id: number = 0;
+    regras_de_fe?: number[];
+    tipo_hinario?: string = '';
+    
+    constructor(data: string = '', numero: number = 0){    
+        if(data) {
+            this.datareuniaoStr = data;
+            this.datareuniao = parseISO(data);
+        }
+
+        this.numero = numero;
+    }
+}
